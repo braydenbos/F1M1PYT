db076c3484f584625138b1477a9d0fafdf3c01ce
Brayden Bos <32762@ma-web.nl>
2021-10-13 19:55:26 +0200

    sixth commit

--- 1.4 TheFinalCountdown.py
+++ 1.4 TheFinalCountdown.py
@@ -1,6 +1,6 @@
 import time
 from PIL import Image
-image = Image.open('C:\\Users\\bosbr\\OneDrive\\Afbeeldingen\\obama.png')
+image = Image.open('C:\\Users\\bosbr\\OneDrive\\Bureaublad\\MA\\Bewijzenmap\\Periode 1.1\\PYT\\obama.png')
 i = 10
 while i > 0:
   print(i)
